690fe70d14d6d3b31f38be796ad28d40380b7e37
Rafael Herik <rafaelherik@gmail.com>
2023-02-03 16:17:35 +0100

    simplify the aks module and add file structure

--- src/modules/aks/variables.tf
+++ src/modules/aks/variables.tf
@@ -1,8 +1,3 @@
-# Global configuration
-variable "global_settings" {
-  description = "Global settings object (see module README.md)"
-}
-
 # Base configuration of the cluster
 variable "name" {
     description = "The AKS cluster name."
@@ -22,8 +17,8 @@ variable "role_based_access_control_enabled" {
 }
 
 # Dynamic configuration of the cluster (Complex objects)
-variable "client_config" {}
-variable "diagnostics" {}
+variable "client_config" {default = {}}
+variable "diagnostics" {default = {}}
 variable "settings" {
     description = "Settings of the AKS Cluster"
     default = {
@@ -31,24 +26,4 @@ variable "settings" {
             name: "default"
         }
     }
-}
-variable "vnets" {}
-variable "admin_group_object_ids" {}
-
-variable "base_tags" {
-  description = "Base tags for the resource to be inherited from the resource group."
-  type        = map(any)
-}
-
-variable "diagnostic_profiles" {
-  default = {}
-}
-variable "private_dns_zone_id" {
-  default = null
-}
-variable "managed_identities" {
-  default = {}
-}
-variable "application_gateway" {
-  default = {}
 }
