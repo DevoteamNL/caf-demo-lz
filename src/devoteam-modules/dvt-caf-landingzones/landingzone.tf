82f3f2c618e4407e2beef3acf94fac2174105597
Rafael Herik <rafaelherik@gmail.com>
2023-02-17 10:09:21 +0100

    Update pipeline

--- src/devoteam-modules/dvt-caf-landingzones/landingzone.tf
+++ src/devoteam-modules/dvt-caf-landingzones/landingzone.tf
@@ -5,9 +5,9 @@ module "landingzone" {
   }   
   current_landingzone_key               = var.landingzone.key  
   global_settings                       = var.global_settings  
-  logged_user_objectId                  = var.backend.client_id
+  logged_user_objectId                  = try(var.backend.client_id, var.azurerm_client_id)
   subscriptions                         = var.subscriptions  
-  tenant_id                             = var.backend.tenant_id 
+  tenant_id                             = try(var.backend.tenant_id, var.tenant_id) 
   resource_groups                       = var.resource_groups
   storage_accounts                      = var.storage_accounts
   networking                            = var.networking 
