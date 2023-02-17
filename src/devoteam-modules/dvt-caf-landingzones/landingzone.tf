0be07de1833899df96597a5643630204c2272da8
Rafael Herik <rafaelherik@gmail.com>
2023-02-17 10:26:41 +0100

    Update provider configuration

--- src/devoteam-modules/dvt-caf-landingzones/landingzone.tf
+++ src/devoteam-modules/dvt-caf-landingzones/landingzone.tf
@@ -8,9 +8,13 @@ module "landingzone" {
   logged_user_objectId                  = try(var.backend.client_id, var.azurerm_client_id)
   subscriptions                         = var.subscriptions  
   tenant_id                             = try(var.backend.tenant_id, var.tenant_id) 
+  subscription_id                       = try(var.backend.subscription_id, var.subscription_id)  
+  client_id                             = try(var.backend.client_id, var.azurerm_client_id)
+  client_secret                         = try(var.backend.client_secret, var.azurerm_client_secret)
   resource_groups                       = var.resource_groups
   storage_accounts                      = var.storage_accounts
   networking                            = var.networking 
   compute                               = var.compute
   tfstates                              = var.landingzone.tfstates
+
 }
--- src/devoteam-modules/dvt-caf/main.tf
+++ src/devoteam-modules/dvt-caf/main.tf
@@ -32,7 +32,10 @@ terraform {
 }
 
 provider "azurerm" {
-    
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.azurerm_client_id
+  client_secret   = var.azurerm_client_secret
     features {}
 }
 
@@ -41,14 +44,6 @@ data "azurerm_subscription" "primary" {
 
 data "azurerm_client_config" "current" {    
 }
-
-# The rover handle the identity management transition to cover interactive run and execution on pipelines using azure ad applications or managed identities
-# There are different scenrios are considered:
-#
-# 1 - running launchpad from vscode
-#  In this bootstrap scenario the launchpad is executed under a logged in user azure session. The rover sets the logged_user_objectId through environment variable. During that initial run an Azure AD application (refered as launchpad_app_level0) is created to support any execution from a pipeline.
-# 2 - deploying a landing zone or a solution from vscode
-#  Step 1 has been executed. The rover is still connected to a logged in user azure session. The rover use the user's credentials to connect the default azure subscription to identity the storage account and the keyvault holding the tfstate and the launchpad_app_level0 credentials. The rover set the terraform ARM_* variables to change the terraform provider Azure context (client id, secret, tenant and subscription). The logged_aad_app_objectId is set to the launchpad_app_level0's client_id. Note in that scenario the azure session does not change. Meaning when terraform execute some local execution scripts they are executed in the context of the logged_in_user and not the azure ad application. To simulate from vscode the execution of a local exec with the launchpad_app_level0 credentials, the rover must be executed with the parameter --impersonate (cannot be used during the launchpad initial deployment and destruciton)
 
 
 data "azuread_service_principal" "logged_in_app" {
